8168770ac5fb84a0a0693968e4cc17139f3268b4
Joshua Forman <cyborgforman@gmail.com>
2019-11-24 20:28:30 -0800

    add two horizontal bars with name title template

--- src/main.js
+++ src/main.js
@@ -2,3 +2,19 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+
+
+$(document).ready(function(){
+  $("#numberButton1").click(function(event) {
+    event.preventDefault();
+
+    const numberInput1 = $("#numberInput1").val();
+    $('#zProgress1').animate({ width: numberInput1 + "%"});
+  });
+
+
+  $("#numberButton2").click(function() {
+    const numberInput2 = $("#numberInput2").val();
+    $('#zProgress2').animate({ width: numberInput2 + "%"});
+  });
+});
